dc08e154dd05e2b8414f35cd3d5e8d44e2d3f8db
LeonardoAV7 <aguilarvalarezo7@gmail.com>
2019-04-15 16:51:33 -0500

    presentacion2
    git commit -a -m presentacion2
    git commit -a -m presentacion2

---
+++ presentacion2.py
@@ -0,0 +1,12 @@
+"""
+	Este es mi tecer ejemplos
+	en lenguajje python 
+
+	Ingreso de informacion por teclado
+
+"""
+
+nombre = input ("Por favor ingrese su nombre\n")
+apellido = input ("Por favor ingrese su apellido\n")
+
+print("Mi nombre es: %s\n \tMi apellido es: \n %s"% (nombre, apellido))
